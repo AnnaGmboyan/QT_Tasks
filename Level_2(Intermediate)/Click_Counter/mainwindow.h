dc2206899a9f89a351898c690c26ab833707bf8d
AnnaGmboyan <Annagmboyan1@gmail.com>
2024-02-29 11:39:07 +0400

    Task2

---
+++ Level_2(Intermediate)/Click_Counter/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QVBoxLayout* layout;
+    QLabel* label;
+    QPushButton*  button;
+    int count;
+
+private slots:
+    void increment();
+
+};
+#endif // MAINWINDOW_H
